1418205c6162fb0281d8c5c52fb427642e996824
Magda0006 <lenamagdamaria@gmail.com>
2026-04-07 20:11:42 +0200

    Added footer element to App.js  file
    also added links redirecting to GitHub

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import "./App.css";
 
 function App() {
+  return (
+    <div className="App-container-fluid">
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Magda0006" target="blank">
+          Magda Tudora
+        </a>{" "}
+        and it is open sourced on{" "}
+        <a
+          href="https://github.com/Magda0006/React-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
