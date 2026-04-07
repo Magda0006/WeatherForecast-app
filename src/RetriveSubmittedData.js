5322a2edb721eb2039ff5a5f71a4cbf5e6fefd55
Magda0006 <lenamagdamaria@gmail.com>
2026-04-07 21:18:01 +0200

    Retriving City from form | added API URL parameter
    Created 4 interconnecting functions that retrive the value (the city) from the form input and set it as a constant to be then used as a parameter in the API URL so that everytime the city changes we make a new API call and retrive updated forecast data.
    Used UseState() to set the city value every time we get a new value from the search bar input inside the function handleCity()
    Created function handleSubmit() to make a new API call everytime the submit button clicked
    the function getCIty() makes the API call and then calls the function handleApiResponse() witch logs the API response inside the console.

--- src/RetriveSubmittedData.js
+++ src/RetriveSubmittedData.js
@@ -1,5 +1,26 @@
 import axios from "axios";
+import React, { useState } from "react";
 export default function RetriveSubmittedData(props) {
+  const [city, setCity] = useState(props.defaultCity);
+  function getCity() {
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=2td2983oa81aa7bb308858f488f7ba0c&units=${unit}`;
+    axios.get(apiUrl).then(handleApiResponse);
+  }
+
+  function handleApiResponse(response) {
+    console.log(response);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    getCity();
+  }
+
+  function handleCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
     return (
       <div>
         <form onSubmit={handleSubmit}>
@@ -10,6 +31,7 @@ export default function RetriveSubmittedData(props) {
                 placeholder="Enter a city..."
                 type="search"
                 autoFocus="on"
+                onChange={handleCity}
               ></input>
             </div>
             <div className="col-sm-3">
