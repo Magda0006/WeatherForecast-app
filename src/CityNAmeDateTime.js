5559f7dacf82ede69514930a6acddc458496cf06
Magda0006 <lenamagdamaria@gmail.com>
2026-04-08 15:55:32 +0200

    CityNameDateTime.js | formatted date
    created function time(): receiving the Unix timestamp from the API, we use this function to format the date using new Date(). Returning HTML snippet With the City name the date and the weather condition using the data received from the API.

--- src/CityNAmeDateTime.js
+++ src/CityNAmeDateTime.js
@@ -1,3 +1,43 @@
 function Title(props) {
+  const forecast = props.forecast;
+  console.log(forecast);
+
+  function time(time) {
+    let date = new Date(time * 1000);
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
+  }
+
+  const currentDay = time(forecast.today.time);
+  return (
+    <div>
+      <h1 className="text-capitalize">{forecast.city}</h1>
+      <ul>
+        <li>
+          <div>{currentDay}</div>
+        </li>
+        <li className="text-capitalize">{forecast.today.description}</li>
+      </ul>
+    </div>
+  );
 }
 export default Title;
