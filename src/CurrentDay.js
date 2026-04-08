f92ea1c0913757c59ad17d23ea15195af4134042
Magda0006 <lenamagdamaria@gmail.com>
2026-04-08 16:06:51 +0200

    CurrentDay.js | created switch C°/ F°
    created getFarenheitTemperature() to calculate the F° temp.
    wrapped the C° and the F° into an <a></a> element to be able to add an event listener so that anytime they are clicked they call a function:
    created the functions changeToFarenheit()/changeToCelsius() both functions serve to change the API url parameter of "unit" to either "metric" or "imperial".
    added an if statement to the return to switch between the active links based on what the unit is at the time the CurrentDay() function is being called.

--- src/CurrentDay.js
+++ src/CurrentDay.js
@@ -1,3 +1,68 @@
 function CurrentDay(props) {
+  const forecast = props.forecast;
+
+  function changeToFahrenheit(event) {
+    event.preventDefault();
+    props.setUnit("imperial");
+  }
+  function changeToCelsius(event) {
+    event.preventDefault();
+    props.setUnit("metric");
+  }
+  function getFahrenheitTemperature() {
+    let celsius = forecast.today.temperature;
+    let fahrenheitTemperature = Math.round((celsius * 9) / 5 + 32);
+    return fahrenheitTemperature;
+  }
+  if (props.unit === "metric") {
+    return (
+      <div className="row">
+        <div className="col-6">
+          <img
+            className=""
+            alt={forecast.today.description}
+            src={forecast.today.icon}
+          />
+          <span>
+            {Math.round(forecast.today.temperature)} C° |{" "}
+            <a href="/" onClick={changeToFahrenheit}>
+              F°
+            </a>
+          </span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {forecast.today.humidity}%</li>
+            <li>Wind: {forecast.today.wind} Km/h</li>
+          </ul>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="row">
+        <div className="col-6">
+          <img
+            className=""
+            alt={forecast.today.description}
+            src={forecast.today.icon}
+          />
+          <span>
+            {getFahrenheitTemperature()}{" "}
+            <a href="/" onClick={changeToCelsius}>
+              C°{" "}
+            </a>
+            | F°
+          </span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {forecast.today.humidity}%</li>
+            <li>Wind: {forecast.today.wind} Km/h</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 export default CurrentDay;
