5dc336c87778ae80baf4a3b175c3ca634a688cb6
Magda0006 <lenamagdamaria@gmail.com>
2026-04-08 16:14:47 +0200

    Forecast.js | used map() to return each day
    compiled each forecast day into an object called forecastDays,
    used map() to repeat the HTML structure for each day inside my forecastDays object.

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,3 +1,42 @@
 function Forecast(props) {
+  const forecast = props.forecast;
+  const forecastDays = [
+    forecast.day2,
+    forecast.day3,
+    forecast.day4,
+    forecast.day5,
+    forecast.day6,
+    forecast.day7,
+  ];
+  function time(time) {
+    let date = new Date(time * 1000);
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[date.getDay()];
+  }
+
+  return (
+    <div className="row forecast text-center">
+      {forecastDays.map((day) => (
+        <div className="col">
+          <div>{time(day.time)}</div>
+          <div>
+            <img
+              className="forecastIcon"
+              alt={day.description}
+              src={day.icon}
+            ></img>
+          </div>
+          <div>
+            {Math.round(day.maxTemp)}°
+            <span className="max-forecast-temp">
+              {" "}
+              {Math.round(day.minTemp)}°
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 export default Forecast;
